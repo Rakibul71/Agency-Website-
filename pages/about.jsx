192a8f6c5207d4c920d9318a2b20c521e5775db8
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-06 10:29:07 +0600

    initial next js setup

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+        <meta name="description" content="This is Rakib agency page " />
+      </Head>
+      <h1>This is about page</h1>
+    </div>
+  );
+};
+
+export default about;
