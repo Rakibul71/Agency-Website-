08d0b7cde7e21319ed14046c43090bfce11c15fa
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-08 14:28:25 +0600

    intro reponsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         </li>
 
         <li className={styles.listItem}>
-          <Link href="/products/contact">CONTACT</Link>
+          <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
     </div>
