65cc88df2c62b048b97fe11b1e0f755d2ff586eb
Rakibul71 <rakibulislam202019@gmail.com>
2022-06-08 15:19:23 +0600

    hameburger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className={styles.container}>
       <Link href="/">AV0CAD0</Link>
@@ -24,6 +25,35 @@ const Navbar = () => {
         </li>
 
         <li className={styles.listItem}>
+          <Link href="/contact">CONTACT</Link>
+        </li>
+      </ul>
+      <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+        <div className={styles.line} />
+        <div className={styles.line} />
+        <div className={styles.line} />
+      </div>
+      <ul
+        onClick={() => setOpen(false)}
+        className={styles.menu}
+        style={{ right: open ? "0px" : "-50vw" }}
+      >
+        <li className={styles.menuItem}>
+          <Link href="/">AV0CAD0</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href="/products/design">DESIGN</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href="/products/development">DEVELOPMENT</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href="/products/production">PRODUCTION</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href="/products/photography">PHOTOGRAPHY</Link>
+        </li>
+        <li className={styles.menuItem}>
           <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
